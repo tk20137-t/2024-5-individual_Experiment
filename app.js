ce493ff1d116e92caf6bacd878c2939e95148bff
tk20137 <mikoshun-1013@docomo.ne.jp>
2024-05-25 14:21:29 +0900

    corsの追加

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { Client } = require('pg');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
