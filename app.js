dc180bc8576563b313005d08017ca7027241657b
tk20137 <mikoshun-1013@docomo.ne.jp>
2024-05-25 22:11:00 +0900

    あ

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.delete('/delete-subject/:id', (req, res) => {
   const subjectID = req.params.id;
   console.log(`Deleting subject with ID: ${subjectID}`); // デバッグログ追加
   const query5 = {
-    text: "DELETE FROM 科目表 WHERE 科目ID = $1" 
+    text: "DELETE FROM 科目表 WHERE 科目ID = '$1'" 
   };
 
   client.query(query5, [subjectID])
